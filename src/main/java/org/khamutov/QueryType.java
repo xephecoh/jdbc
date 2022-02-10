fe17a3fed101e61144dd51a1185a33524f121a0b
xephecoh <xephecoh@gmail.com>
2022-02-11 01:53:06 +0200

    refactor all code

---
+++ src/main/java/org/khamutov/QueryType.java
@@ -0,0 +1,15 @@
+package org.khamutov;
+
+public enum QueryType {
+    SELECT("SELECTED"),DELETE("deleted"),UPDATE("updated"),INSERT("inserted");
+
+    final String participle;
+
+    public String getParticiple() {
+        return participle;
+    }
+
+    QueryType(String participle) {
+        this.participle = participle;
+    }
+}
